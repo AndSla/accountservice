743fbff7fd8febd855f1c549978fa3cafef40a99
Andrzej <annddrzej@gmail.com>
2022-07-28 18:27:40 +0200

    Change display of period and salary in payment response

--- src/main/java/com/learning/accountservice/model/response/PaymentResponse.java
+++ src/main/java/com/learning/accountservice/model/response/PaymentResponse.java
@@ -1,10 +1,14 @@
 package com.learning.accountservice.model.response;
 
+import java.math.BigDecimal;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
 public class PaymentResponse {
     private String name;
     private String lastName;
     private String period;
-    private Long salary;
+    private String salary;
 
     public PaymentResponse() {
     }
@@ -30,14 +34,21 @@ public class PaymentResponse {
     }
 
     public void setPeriod(String period) {
-        this.period = period;
+        DateTimeFormatter formatterIn = DateTimeFormatter.ofPattern("MM-yyyy");
+        DateTimeFormatter formatterOut = DateTimeFormatter.ofPattern("MMMM-yyyy");
+        YearMonth monthYear = YearMonth.parse(period, formatterIn);
+        this.period = monthYear.format(formatterOut);
     }
 
-    public Long getSalary() {
+    public String getSalary() {
         return salary;
     }
 
     public void setSalary(Long salary) {
-        this.salary = salary;
+        BigDecimal bd = new BigDecimal(salary).movePointLeft(2);
+        String[] amount = bd.toString().split("\\.");
+        String dollars = amount[0];
+        String cents = amount[1];
+        this.salary = dollars + " dollar(s) " + cents + " cent(s)";
     }
 }
